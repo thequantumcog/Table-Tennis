27367c004b08dbcb60847fe69c3df09bac54e2f6
Quantum Cog <thequantumcog@protonmail.com>
2024-12-06 11:52:14 +0500

    add Multiple balls for fun ;)

--- src/game.cpp
+++ src/game.cpp
@@ -1,17 +1,22 @@
 #include "main.hpp"
+#include <raylib.h>
 
+const int nballs=5;
 bool gameover = 0;
 bool gamePaused=0;
 bool scoreAdded = 0;
 Paddle paddle = { { screenWidth - paddleRadius - wallSize, screenHeight / 2.0f }, 700.0f, 59.5f, BLACK, 0 }; // pos, size, speed, color, hit
-Ball ball = { { paddle.pos.x, paddle.pos.y }, BallSpeed, 20.0f, PURPLE, 0, 0 }; // pos, speed, radius, color, visible
+Ball ball[5] = {{{ paddle.pos.x, paddle.pos.y }, BallSpeed, 20.0f, PURPLE, 0, 0 },{{ paddle.pos.x, paddle.pos.y }, BallSpeed, 20.0f, PURPLE, 0, 0 },{{ paddle.pos.x, paddle.pos.y }, BallSpeed, 20.0f, PURPLE, 0, 0 }
+,{{ paddle.pos.x, paddle.pos.y }, BallSpeed, 20.0f, PURPLE, 0, 0 }
+,{{ paddle.pos.x, paddle.pos.y }, BallSpeed, 20.0f, PURPLE, 0, 0 }
+}; // pos, speed, radius, color, visible
 double deltaTime = 0;
 double lasthit = 0.0;
 bool moveSelector=0;
 
 
 void checkCollision(Ball &ball, Paddle &paddle, int &score, double &lasthit);
-void takeGameInput(Paddle &paddle, Ball &ball, double &lasthit, double deltaTime, State &gameState);
+void takeGameInput(Paddle &paddle, Ball * ball, double &lasthit, double deltaTime, State &gameState);
 void drawGame(int &score, State &gameState,bool &exitWindow);
 // START
 void beginGame(int &score, State &gameState,bool &exitWindow) {
@@ -35,23 +40,25 @@ void beginGame(int &score, State &gameState,bool &exitWindow) {
 
     if (!gameover && !gamePaused) {
         takeGameInput(paddle, ball, lasthit, deltaTime, gameState);
-        if (ball.thrown) {
-            checkCollision(ball, paddle, score, lasthit);
-            if (ball.pos.x - (wallSize + ball.radius) <= 0){
-                ball.speed.x *= -1;
+        for(int i=0;i<nballs;i++){
+        if (ball[i].thrown) {
+                checkCollision(ball[i], paddle, score, lasthit);
+            if (ball[i].pos.x - (wallSize + ball[i].radius) <= 0){
+                ball[i].speed.x *= -1;
                 PlaySound(hit);
             }
-            if (ball.pos.y - (wallSize + 10.0f + ball.radius) <= 0 || ball.pos.y + (wallSize + 10.0f + ball.radius) >= screenHeight){
-                ball.speed.y *= -1;
+            if (ball[i].pos.y - (wallSize + 10.0f + ball[i].radius) <= 0 || ball[i].pos.y + (wallSize + 10.0f + ball[i].radius) >= screenHeight){
+                ball[i].speed.y *= -1;
                 PlaySound(hit2);
             }
-            ball.pos.y += ball.speed.y * deltaTime;
-            ball.pos.x -= ball.speed.x * deltaTime;
-            if (ball.pos.x > screenWidth + 100) { // 100 is added here to add a delay in gameover
-                ball.thrown = 0;
+            ball[i].pos.y += ball[i].speed.y * deltaTime;
+            ball[i].pos.x -= ball[i].speed.x * deltaTime;
+            if (ball[i].pos.x > screenWidth - wallSize - ball[i].radius) { // 100 is added here to add a delay in gameover
+                ball[i].thrown = 0;
                 gameover = 1;
-                ball = { { paddle.pos.x, paddle.pos.y }, { 500.0f, 0.0f }, 20.0f, PURPLE, 0 }; // pos, speed, radius, color, visible
+                ball[i] = { { paddle.pos.x, paddle.pos.y }, { 500.0f, 0.0f }, 20.0f, PURPLE, 0 }; // pos, speed, radius, color, visible
             }
+        }
         }
     }
     else if(!gamePaused && gameover){
@@ -80,19 +87,21 @@ void checkCollision(Ball &ball, Paddle &paddle, int &score, double &lasthit) {
     }
 }
 
-void takeGameInput(Paddle &paddle, Ball &ball, double &lasthit, double deltaTime, State &gameState) {
+void takeGameInput(Paddle &paddle, Ball * ball, double &lasthit, double deltaTime, State &gameState) {
     if (IsKeyDown(KEY_UP) && paddle.pos.y > wallSize + paddle.radius)
         paddle.pos.y -= paddle.speed * deltaTime;
     if (IsKeyDown(KEY_DOWN) && paddle.pos.y + wallSize < screenHeight - paddle.radius)
         paddle.pos.y += paddle.speed * deltaTime;
 
-    if (IsKeyPressed(KEY_SPACE) && !ball.thrown) {
-        ball.pos.y = paddle.pos.y;
-        ball.speed.x = BallSpeed.x;
-        ball.speed.y = GetRandomValue(-500, 500);
-        ball.thrown = 1;
-        ball.canCollide = 0;
+    for(int i=0;i<nballs;i++){
+    if (IsKeyPressed(KEY_SPACE) && !ball[i].thrown) {
+        ball[i].pos.y = paddle.pos.y;
+        ball[i].speed.x = BallSpeed.x;
+        ball[i].speed.y = GetRandomValue(-500, 500);
+        ball[i].thrown = 1;
+        ball[i].canCollide = 0;
         lasthit = GetTime();
+    }
     }
 }
 
@@ -103,10 +112,11 @@ void drawGame(int &score,State &gameState,bool &exitWindow) {
         DrawTexture(backgroundTexture, 15, 30, WHITE);
         DrawText(TextFormat("FPS: %i", fps), 10, 10, 20, DARKGRAY);
         DrawTexture(paddleTexture, paddle.pos.x - paddle.radius, paddle.pos.y - paddle.radius, WHITE);
-        if (ball.thrown) {
-            DrawTexture(ballTexture, ball.pos.x - ball.radius, ball.pos.y - ball.radius, WHITE);
+    for(int i=0;i<nballs;i++){
+        if (ball[i].thrown) {
+            DrawTexture(ballTexture, ball[i].pos.x - ball[i].radius, ball[i].pos.y - ball[i].radius, WHITE);
             DrawText(TextFormat("%02d", score), screenWidth / 2.0f - 35, 25, 60, scoreColor);
-        }
+        }}
      if(gameover) {
         DrawTexture(nameplate, screenWidth/2.0f-270, screenHeight/2.0f-170, WHITE);
         DrawText("GAME OVER!", screenWidth / 2.0f - MeasureText("GAME OVER!", 40)/2.0f, screenHeight / 2.0f - 150.0f, 50, YELLOW);
@@ -127,8 +137,9 @@ void drawGame(int &score,State &gameState,bool &exitWindow) {
             DrawTexture(selector,screenWidth / 2.0f + MeasureText("No", 40)/2.0f+ 50.0f, screenHeight / 2.0f + 55.0f,WHITE);
         if(IsKeyReleased(KEY_ENTER)){
                 paddle = { { screenWidth - paddleRadius - wallSize, screenHeight / 2.0f }, 700.0f, 59.5f, BLACK, 0 }; // pos, size, speed, color, hit
-                ball = { { paddle.pos.x, paddle.pos.y }, { 500.0f, 0.0f }, 20.0f, PURPLE, 0 }; // pos, speed, radius, color, visible
-                score =0;
+            for(int i=0;i<nballs;i++)
+                ball[i] = { { paddle.pos.x, paddle.pos.y }, { 500.0f, 0.0f }, 20.0f, PURPLE, 0 }; // pos, speed, radius, color, visible
+            score =0;
                 scoreAdded=0;
             if(moveSelector){
                 gameState=MENU;
@@ -139,6 +150,7 @@ void drawGame(int &score,State &gameState,bool &exitWindow) {
             }
         }
     }
-    if(gamePaused && !gameover) drawPauseMenu(gameState,gamePaused,score,ball,paddle,exitWindow);
+    for(int i=0;i<nballs;i++)
+    if(gamePaused && !gameover) drawPauseMenu(gameState,gamePaused,score,ball[i],paddle,exitWindow);
 }
 
